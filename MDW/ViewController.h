1420d0f8285d7740edeae7b11f94da4098522f8b
marko <marko@markos-iMac.local>
2017-04-13 14:19:17 +0200

    first commit

---
+++ MDW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MDW
+//
+//  Created by marko on 4/12/17.
+//  Copyright © 2017 marko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Realm/Realm.h>
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, ‘8.0’
+
+target 'MDW' do
+
+pod 'AFNetworking', '~> 3.0'
+pod 'SDWebImage', '~>3.8'
+pod 'ZXingObjC', '~> 3.2.1'
+pod 'Realm' , '~> 2.3.0’
+end
