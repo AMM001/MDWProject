36ab50343274db53b35643bb6a468bf5ae489914
marko <marko@markos-imac.local>
2017-04-25 14:58:33 +0200

    all days agenda rendering

--- AllDaysAgendaViewController.m
+++ AllDaysAgendaViewController.m
@@ -7,7 +7,9 @@
 //
 
 #import "AllDaysAgendaViewController.h"
-
+#import "MDWNetworkManager.h"
+#import "SessionDTO.h"
+#import "DateConverter.h"
 @interface AllDaysAgendaViewController ()
 
 @end
@@ -16,6 +18,107 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _allSessions = [NSMutableArray new];
+    [MDWNetworkManager fetchAllSessionsData:_allSessions :_AllDaysTable];
+    
+    
+    
+    
+    
+    
+    
+//    SessionDTO *s = [SessionDTO new];
+//    s.sessionType=@"type 1";
+//    s.name=@"s1";
+//    s.id=1;
+//    s.liked = true;
+//    s.sessionDescription=@"sdjkfdsjkbfjdsbfbdsfsdns";
+//    SpeakerDTO *sp = [SpeakerDTO new];
+//    sp.id=1;
+//    sp.firstName=@"markosp";
+//    RLMString *ss =[RLMString new];
+//    ss.string = @"012545";
+//    [sp.mobiles addObject:ss];
+//    ss =[RLMString new];
+//    ss.string = @"012545";
+//    [sp.mobiles addObject:ss];
+//    sp.gender=true;
+//    [s.speakers addObject:sp];
+//    sp = [SpeakerDTO new];
+//    sp.id=2;
+//    sp.firstName=@"samysp";
+//    
+//    ss =[RLMString new];
+//    ss.string = @"012545";
+//    [sp.mobiles addObject:ss];
+//    ss =[RLMString new];
+//    ss.string = @"012545";
+//    [sp.mobiles addObject:ss];
+//    sp.gender=true;
+//    [s.speakers addObject:sp];
+//    sp = [SpeakerDTO new];
+//    sp.id=3;
+//    sp.firstName=@"eslamsp";
+//    ss =[RLMString new];
+//    ss.string = @"012545";
+//    [sp.mobiles addObject:ss];
+//    ss =[RLMString new];
+//    ss.string = @"012545";
+//    [sp.mobiles addObject:ss];
+//    sp.gender=true;
+//    [s.speakers addObject:sp];
+//    [_allSessions addObject:s];
+//    
+//    s = [SessionDTO new];
+//    s.sessionType=@"type 2";
+//    s.name=@"s2";
+//    s.id=2;
+//    s.liked = true;
+//    sp = [SpeakerDTO new];
+//    sp.id=4;
+//    sp.firstName=@"markosp";
+//    ss =[RLMString new];
+//    ss.string = @"012545";
+//    [sp.mobiles addObject:ss];
+//    ss =[RLMString new];
+//    ss.string = @"012545";
+//    [sp.mobiles addObject:ss];
+//    sp.gender=true;
+//    [s.speakers addObject:sp];
+//    sp = [SpeakerDTO new];
+//    sp.id=5;
+//    sp.firstName=@"samysp";
+//    ss =[RLMString new];
+//    ss.string = @"012545";
+//    [sp.mobiles addObject:ss];
+//    ss =[RLMString new];
+//    ss.string = @"012545";
+//    [sp.mobiles addObject:ss];
+//    sp.gender=true;
+//    [s.speakers addObject:sp];
+//    sp = [SpeakerDTO new];
+//    sp.id=6;
+//    sp.firstName=@"eslamsp";
+//    ss =[RLMString new];
+//    ss.string = @"012545";
+//    [sp.mobiles addObject:ss];
+//    ss =[RLMString new];
+//    ss.string = @"012545";
+//    [sp.mobiles addObject:ss];
+//    sp.gender=true;
+//    [s.speakers addObject:sp];
+//    [_allSessions addObject:s];
+//
+//    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     _barButton.target=self.revealViewController;
     _barButton.action=@selector(revealToggle:);
     
@@ -31,13 +134,15 @@
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 3;
+    return [_allSessions count];
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
-    
+    static NSString *cellIdentifier =@"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if(cell == nil){
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
     
     UILabel *firstLabel = (UILabel*) [cell viewWithTag:2];
     UILabel *secondLabel = (UILabel*) [cell viewWithTag:3];
@@ -45,9 +150,17 @@
     UIImageView *imageView = (UIImageView*) [cell viewWithTag:5];
     
     
-    firstLabel.text = @"First Label";
-    secondLabel.text = @"Second Label";
-    thirdLabel.text = @"Third Label";
+//    firstLabel.text = @"First Label";
+//    secondLabel.text = @"Second Label";
+//    thirdLabel.text = @"Third Label";
+    SessionDTO * sessionToView =nil;
+    sessionToView=[_allSessions objectAtIndex:indexPath.row];
+    firstLabel.text = [sessionToView name];
+    secondLabel.text = [sessionToView location];
+    NSString * date = [NSString stringWithFormat:@"%@ - %@",
+                       [DateConverter stringFromDate:sessionToView.startDate],
+                       [DateConverter stringFromDate:sessionToView.endDate]];
+    thirdLabel.text = date;
     imageView.image=[UIImage imageNamed:@"myagenda.png"];
     
     return cell;
